6099484f4161cc9868e2e3b1e441ca6d4d5ba0fe
Martin <martin@rockin.dev>
2023-09-03 14:49:09 -0300

    Vue use added

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Home</h1>
+  <span>{{ `x = ${x}, y = ${y}` }}</span>
+</template>
+
+<script setup lang="ts">
+import { useMouse } from '@vueuse/core'
+
+const {x, y } = useMouse()
+</script>
