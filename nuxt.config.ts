b4408048a78c34ffd371be7e56db2150e87eaa02
Martin <martin@rockin.dev>
2023-09-03 11:36:15 -0300

    SSR enabled + static server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   runtimeConfig: {
     public: {
       APP_BASE_URL: process.env.APP_BASE_URL
